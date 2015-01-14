a5415d10c0bb9f378e8c2ee802840ece783ec831
Olekasndr Pavloshchuk <ap730518@gmail.com>
2015-01-14 16:25:12 +0200

    added some fields; there are some errors here

--- src/main/java/org/slowpoke/arithmetics/DivideGreater.java
+++ src/main/java/org/slowpoke/arithmetics/DivideGreater.java
@@ -1,10 +1,16 @@
 package org.slowpoke.arithmetics;
 
+import static org.slowpoke.arithmetics.VeryLargeNumber.mul;
+
 public class DivideGreater extends BinaryOperation {
 
 	private final int precision;
 
+	private final String[] digitProducts = new String[10];
+	private Number lastProbe = null;
+
 	DivideGreater(int precision) {
+		this.digitProducts[0] = "0";
 		this.precision = 0 > precision ? 0 : precision;
 	}
 
@@ -21,20 +27,29 @@ public class DivideGreater extends BinaryOperation {
 			result.setSign(-result.getSign());
 			return result;
 		}
-		
-		
+
 		final int compareAbs = n1.compareAbs(n2);
 		final int sign = n1.getSign() * n2.getSign();
-		
-		if( 0>compareAbs) {
-			throw new IllegalArgumentException("1st argument by module is less that 2nd one");
-		} else if (0==compareAbs ) {
+
+		if (0 > compareAbs) {
+			throw new IllegalArgumentException(
+					"1st argument by module is less that 2nd one");
+		} else if (0 == compareAbs) {
 			return new Factory().createFrom("1").setSign(sign);
 		}
-		
-		Number result = new Number();
 
-		// TODO Auto-generated method stub
+		prepareDigitProducts(n2);
+
+		Number result = new Number();
+		final int orderDiff = getOrderDifference(n1, n2);
+		Number rest = n1;
+		for (int order = orderDiff; order >= 0; order--) {
+			int digit = findDigit(rest, order);
+			result.addDigit(digit);
+			rest = new SubtractAbs().perform(rest, lastProbe);
+		}
+
+		// TODO: what we have to do with the rest and how to normalize digits and precision?
 
 		int pointOffset = n1.getPointOffset() - n2.getPointOffset() + precision;
 		if (0 > pointOffset) {
@@ -45,6 +60,30 @@ public class DivideGreater extends BinaryOperation {
 		}
 		result.setSign(sign);
 		return result;
+	}
+
+	private void prepareDigitProducts(Number n) {
+		for (int i = 1; i < this.digitProducts.length; i++) {
+			this.digitProducts[i] = mul(n.toString(), String.valueOf(i));
+		}
+	}
+
+	private int findDigit(Number n, int order) {
+		for (int i = 9; i >= 0; i--) {
+			this.lastProbe = new Factory().createFrom(this.digitProducts[i])
+					.multiplyTo10(order);
+			if (0 >= this.lastProbe.compareAbs(n)) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	// Difference of the number orders
+	private int getOrderDifference(Number n1, Number n2) {
+		int size1 = n1.getDigitsCount() - n1.getPointOffset();
+		int size2 = n2.getDigitsCount() - n2.getPointOffset();
+		return size1 - size2;
 	}
 
 }
--- src/test/java/org/slowpoke/arithmetics/DivideGreaterUnitTest.java
+++ src/test/java/org/slowpoke/arithmetics/DivideGreaterUnitTest.java
@@ -13,6 +13,12 @@ public class DivideGreaterUnitTest {
 		assertEquals("", div("-6782.80823", "something dull", 0));
 		assertEquals("", div("0.1782", "0", 0));
 	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void whenDivideLessThatThrowException() {
+		assertEquals("", div("89", "120", 0));
+		assertEquals("", div("-0.0023872", "16", 0));
+	}	
 
 	@Test
 	public void divToPredefined() {
@@ -22,6 +28,13 @@ public class DivideGreaterUnitTest {
 		assertEquals("-1", div("-9000", "9000", 0));
 		assertEquals("1", div("78.000", "78", 0));
 	}
+	
+	@Test
+	public void divExact() {
+		assertEquals("2", div("4", "2", 0));
+// TODO wrong value! Should be 8, but now is 80		
+//		assertEquals("", div("512", "64", 0));
+	}	
 
 	private static String div(String s1, String s2, int precision) {
 		Number n1 = new Factory().createFrom(s1);
