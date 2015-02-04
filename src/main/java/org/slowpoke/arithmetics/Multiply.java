9eeb2f8157ce7ff8077062e7292ad101f19a1280
Olekasndr Pavloshchuk <ap730518@gmail.com>
2015-02-04 13:56:56 +0200

    cache for products to digits

--- src/main/java/org/slowpoke/arithmetics/Multiply.java
+++ src/main/java/org/slowpoke/arithmetics/Multiply.java
@@ -2,18 +2,29 @@ package org.slowpoke.arithmetics;
 
 class Multiply extends BinaryOperation {
 
-	private final AddAbs helperAdd = new AddAbs();
+	private final AddAbs addAbs = new AddAbs();
+
+	private final Number[] calculatedProducts = new Number[10];
 
 	@Override
 	Number perform(Number n1, Number n2) {
 		Number result = new Number();
 		for (int i = 0; i < n2.getDigitsCount(); i++) {
-			result = helperAdd.perform(multiply(n1, n2.getDigit(i))
-					.multiplyTo10(i), result);
+			result = addAbs.perform(get(n1, n2.getDigit(i)).multiplyTo10(i),
+					result);
 		}
 		result.setSign(n1.getSign() * n2.getSign());
-		result.setPointOffset( n1.getPointOffset() + n2.getPointOffset() );
+		result.setPointOffset(n1.getPointOffset() + n2.getPointOffset());
 		return result;
+	}
+
+	private Number get(Number src, int b) {
+		Number result = calculatedProducts[b];
+		if (null == result) {
+			calculatedProducts[b] = multiply(src, b);
+			result = calculatedProducts[b];
+		}
+		return new Factory().createCopy(result);
 	}
 
 	private Number multiply(Number src, int b) {
