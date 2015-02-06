7afd4f801648b2ee558b2ad2eec6dc8452ca4205
Olekasndr Pavloshchuk <ap730518@gmail.com>
2015-02-06 17:07:20 +0200

    form and report pages were added

--- slowpoke-arithmetics-stress-tests/src/main/java/org/slowpoke/arithmetics/stress/tests/App.java
+++ slowpoke-arithmetics-stress-tests/src/main/java/org/slowpoke/arithmetics/stress/tests/App.java
@@ -1,13 +1,13 @@
 package org.slowpoke.arithmetics.stress.tests;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
--- slowpoke-arithmetics-stress-tests/src/test/java/org/slowpoke/arithmetics/stress/tests/AppTest.java
+++ slowpoke-arithmetics-stress-tests/src/test/java/org/slowpoke/arithmetics/stress/tests/AppTest.java
@@ -1,38 +1,13 @@
 package org.slowpoke.arithmetics.stress.tests;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
+	@Test
+    public void dummy()
     {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
