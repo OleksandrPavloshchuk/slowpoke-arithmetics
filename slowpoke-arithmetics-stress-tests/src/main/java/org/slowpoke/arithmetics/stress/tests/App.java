a92a51e4142d563b6239f5b7fa555e302e90989e
Olekasndr Pavloshchuk <ap730518@gmail.com>
2015-02-06 14:09:28 +0200

    new project for stress tests

---
+++ slowpoke-arithmetics-stress-tests/src/main/java/org/slowpoke/arithmetics/stress/tests/App.java
@@ -0,0 +1,13 @@
+package org.slowpoke.arithmetics.stress.tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ slowpoke-arithmetics/src/main/java/org/slowpoke/arithmetics/Multiply.java
@@ -0,0 +1,42 @@
+package org.slowpoke.arithmetics;
+
+class Multiply extends BinaryOperation {
+
+	private final AddAbs addAbs = new AddAbs();
+
+	private final Number[] calculatedProducts = new Number[10];
+
+	@Override
+	Number perform(Number n1, Number n2) {
+		Number result = new Number();
+		for (int i = 0; i < n2.getDigitsCount(); i++) {
+			result = addAbs.perform(get(n1, n2.getDigit(i)).multiplyTo10(i),
+					result);
+		}
+		result.setSign(n1.getSign() * n2.getSign());
+		result.setPointOffset(n1.getPointOffset() + n2.getPointOffset());
+		return result;
+	}
+
+	private Number get(Number src, int b) {
+		Number result = calculatedProducts[b];
+		if (null == result) {
+			calculatedProducts[b] = multiply(src, b);
+			result = calculatedProducts[b];
+		}
+		return new Factory().createCopy(result);
+	}
+
+	private Number multiply(Number src, int b) {
+		final Number result = new Number();
+		int prev = 0;
+		for (int i = 0; i < src.getDigitsCount(); i++) {
+			final int n = src.getDigit(i) * b + prev;
+			prev = n / 10;
+			result.setDigit(i, n % 10);
+		}
+		result.setDigit(src.getDigitsCount(), prev);
+		return result;
+	}
+
+}
